3c2ddfeb3157d4e6cbe64fadec5cbbfb596cde33
xiaoyu <975440488@qq.com>
2022-12-26 23:19:21 +0800

    add file "LogSend.h"

---
+++ include/LogServer/auxiliary_process/LogSend.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "auxiliary_process/LogSend.pb.h"
+
+#include <muduo/net/TcpServer.h>
+#include <muduo/net/TcpConnection.h>
+#include <muduo/net/EventLoop.h>
+#include <muduo/net/Buffer.h>
+
+#include <string>
+
+class LogSend {
+public:
+    // 开始服务
+    void run(std::string ip, uint16_t port);
+
+    // 读取文件并发送消息，绑定到muduo的on_message消息回调
+    void sendFile(const muduo::net::TcpConnectionPtr& conn, muduo::net::Buffer* buf, muduo::Timestamp stamp);
+
+    // 关闭连接
+    void closeConn(const muduo::net::TcpConnectionPtr& conn);
+
+private:
+    muduo::net::EventLoop loop_;
+};
